9129439ade7979bc02aed64ff6845b552ec176c9
ynatt <Thecruly@gmail.com>
2023-03-04 21:14:20 +0900

    init commit

---
+++ src/main/java/any/mind/pointsystem/PointSystemApplication.java
@@ -0,0 +1,13 @@
+package any.mind.pointsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/any/mind/pointsystem/PointSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package any.mind.pointsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
